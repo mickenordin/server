d73d59df450e17bbdcd841c789159a56d81954c8
Enrique Pérez Arnaud <enrique@cazalla.net>
2026-01-26 11:45:55 +0100

    fix(files_external): Added missing doTryTokenLogin() method to implement the IUserSession interface
    Signed-off-by: Enrique Pérez Arnaud <enrique@cazalla.net>

--- apps/files_external/lib/Migration/DummyUserSession.php
+++ apps/files_external/lib/Migration/DummyUserSession.php
@@ -62,4 +62,8 @@ class DummyUserSession implements IUserSession {
 	public function setImpersonatingUserID(bool $useCurrentUser = true): void {
 		//no OP
 	}
+
+	public function doTryTokenLogin(string $token): bool {
+		return false;
+	}
 }
